51624ee2cf27e8cc3b37be0f4d96e327b2fde6db
Osamah Amer <osku@osamah-dev.tech>
2022-04-27 16:16:22 +0300

    Init ES6 modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    entry:'./src/index.ts',
-    module:{
-        rules: [
-            {
-                test: /\.ts$/,
-                use:'ts-loader',
-                include: [path.resolve(__dirname, 'src')],
-            }
-        ]
-    },
-    output:{
-        publicPath: 'public',
-        filename:'bundle.js',
-        path:path.resolve(__dirname,'public')
-    },devServer: {
-        static: {
-          directory: path.join(__dirname, 'public'),
-        },
-        compress: true,
-        port: 9000,
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
       },
-      mode: 'development'
-      
-}
+    ],
+  },
+  
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+  output: {
+    publicPath: "public",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+  },
+  mode: "development",
+};
