7948bd1ada70a82fdda7be1706759153807ce26f
Osamah Amer <osku@osamah-dev.tech>
2022-04-27 16:03:11 +0300

    Webpack dev server  installed

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,16 @@ module.exports = {
         ]
     },
     output:{
+        publicPath: 'public',
         filename:'bundle.js',
         path:path.resolve(__dirname,'public')
-    }
+    },devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+      },
+      mode: 'development'
+      
 }
